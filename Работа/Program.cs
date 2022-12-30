86de938a09b0395d16527249d05379a574c5eca4
george <georgij5ptb@gmail.com>
2022-12-30 21:56:54 +0300

    Задали функции поиска слов и вывода массива

--- Работа/Program.cs
+++ Работа/Program.cs
@@ -1,2 +1,28 @@
-﻿string[] list={"привет","_+=",":)","world","sun"};
+﻿string[] SravnenieStrok(string[] array)
+{
+    string[] list = new string[array.Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            list[i] = array[i];
+        }
+    }
+    return list;
+}
+void PrintList(string[] list)
+{
+    for (int i = 0; i < list.Length; i++)
+    {
+        System.Console.WriteLine(list[i]);
+        System.Console.WriteLine();
+    }
+}
 
+
+
+
+
+
+
+string[] list = { "привет", "_+=", ":)", "world", "sun" };
